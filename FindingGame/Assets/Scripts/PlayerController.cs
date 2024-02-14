942bb80a912ace6962f5fc5ce35df9a254f787d3
Alina Stec <81440573+Alina-1802@users.noreply.github.com>
2024-02-14 14:54:14 +0100

    Added basic player movement

---
+++ FindingGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float angle = 150.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 movement = new Vector3(0,0,Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * angle * Time.deltaTime, 0);
+        transform.Translate(movement);
+    }
+}
